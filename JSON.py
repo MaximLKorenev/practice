475bdd6957805727fc0a717a94263e9610f095fd
MaximLKorenev <mlkorenev@yandex.ru>
2022-01-26 19:50:48 +0300

    new

--- JSON.py
+++ JSON.py
@@ -7,29 +7,13 @@ result = json.loads(response.text)
 with open("file.json", "w") as json_file:
     json.dump(result, json_file)
 
-n = len(result)
-user_id = result[0]['userId']
-count_user = 1
-num = 0
-completed = 0
-id = 0
-user = {}
+users = {}
+for user in result:
+    user_id = user["userId"]
+    if user_id not in users:
+        users[user_id] = {"num": 0, "completed": 0}
+    users[user_id]["num"] += 1
+    if user["completed"]:
+        users[user_id]["completed"] += 1
 
-for i in range(n):
-
-    if result[i]['userId'] != user_id:
-        user[result[i-1]["userId"]] = {"num": num, "completed": completed}
-        num = 0
-        completed = 0
-        count_user += 1
-        user_id = result[i]['userId']
-
-    if result[i]['id'] != id:
-        num += 1
-
-    if result[i]["completed"]:
-        completed += 1
-
-    if i == n - 1:
-        user[result[i]["userId"]] = {"num": num, "completed": completed}
-
+print(users)
