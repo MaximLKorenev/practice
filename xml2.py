e16e983b3ff7ba77f2d056b27bf290e6dd0ada49
MaximLKorenev <mlkorenev@yandex.ru>
2022-01-30 20:27:45 +0300

    new

--- xml2.py
+++ xml2.py
@@ -1,7 +1,10 @@
-import xml.etree.ElementTree as ETree
+from xml.etree import ElementTree
 
-xml1 = ETree.parse('demo.xml')
-root = xml1.getroot()
+tree = ElementTree.parse('demo.xml').getroot()
+parent_map = {c: p for p in tree.iter() for c in p}
+
+item = tree.find('pc').find('pc_item')
+print(parent_map[item].tag)
 
 
 def remove_all_by_tag(root, tag):
@@ -12,4 +15,3 @@ def remove_all_by_tag(root, tag):
         remove_all_by_tag(subitem, tag)
 
 
-def find_parent(root):
