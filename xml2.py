ad2685aea5dc32e27158805b88f506f4c547fbf1
MaximLKorenev <mlkorenev@yandex.ru>
2022-01-30 20:29:23 +0300

    new

--- xml2.py
+++ xml2.py
@@ -7,11 +7,14 @@ item = tree.find('pc').find('pc_item')
 print(parent_map[item].tag)
 
 
-def remove_all_by_tag(root, tag):
-    if root.tag == tag:
-        root.clear()
-        return
-    for subitem in root.findall(tag):
-        remove_all_by_tag(subitem, tag)
+def remove_nodes_recursive(root, remove_tag):
+    remove_list = []
+    for child in root:
+        if child.tag == remove_tag:
+           remove_list.append(child)
 
+    for child in remove_list:
+        root.remove(child)
 
+    for child in root:
+        remove_nodes_recursive(child, remove_tag)
