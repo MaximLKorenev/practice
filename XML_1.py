3417dbac75279e416009ee9b984baa19fff2fda7
MaximLKorenev <mlkorenev@yandex.ru>
2022-01-30 20:23:46 +0300

    new

--- XML_1.py
+++ XML_1.py
@@ -20,17 +20,3 @@ def tree_count_recursive(root, key):
     for child in root:
         n += tree_count_recursive(child, key)
     return n
-
-
-def remove_all_by_tag(root, tag):
-    if root.tag == tag:
-        root.clear()
-        return
-    for subitem in root.findall(tag):
-        remove_all_by_tag(subitem, tag)
-
-
-def find_parent(root):
-    
-
-
